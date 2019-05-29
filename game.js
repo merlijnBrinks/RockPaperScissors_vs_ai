8c5b8bca92c25bf263be86fb6f04a584b592955b
merlijnBrinks <45800938+merlijnBrinks@users.noreply.github.com>
2019-05-29 13:35:03 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
+
+let streak = 0
+
 function myFunction() {
 
-
+    
 let inputForRockPaperScissors = prompt("choose input: Rock, Paper or Scissors. (use capital)", "Rock")
 // ask for input
 let aiInput = Math.floor((Math.random() * 3) + 1)
@@ -15,15 +18,18 @@ else if (aiInput === 2) {
 else if (aiInput === 3) {
   aiInput = "scissors"
 }
+    
 else {
   document.getElementById("result").innerHTML = "error404: ai Input not found";
 }
 
 if (inputForRockPaperScissors === "Rock" && aiInput === "paper") {
   document.getElementById("result").innerHTML = "You lost";
+    streak = 0;
 }
 else if (inputForRockPaperScissors === "Rock" && aiInput === "scissors") {
   document.getElementById("result").innerHTML = "You won!!!";
+   streak = streak + 1;
 }
 else if (inputForRockPaperScissors === "Rock" && aiInput === "rock") {
   document.getElementById("result").innerHTML = "It's a draw";
@@ -33,15 +39,19 @@ else if (inputForRockPaperScissors === "Paper" && aiInput === "paper") {
 }
 else if (inputForRockPaperScissors === "Paper" && aiInput === "rock") {
   document.getElementById("result").innerHTML = "You won!!!";
+    streak = streak + 1;
 }
 else if (inputForRockPaperScissors === "Paper" && aiInput === "scissors") {
   document.getElementById("result").innerHTML = "You lost";
+    streak = 0;
 }
 else if (inputForRockPaperScissors === "Scissors" && aiInput === "paper") {
   document.getElementById("result").innerHTML = "You won!!!!";
+    streak = streak + 1;
 }
 else if (inputForRockPaperScissors === "Scissors" && aiInput === "rock") {
   document.getElementById("result").innerHTML = "You lost";
+    streak = 0;
 }
 else if (inputForRockPaperScissors === "Scissors" && aiInput === "scissors") {
   document.getElementById("result").innerHTML = "It's a draw!";
@@ -58,8 +68,22 @@ else if (inputForRockPaperScissors === "rock") {
 else if (inputForRockPaperScissors === "scissor") {
   document.getElementById("result").innerHTML = "Please use capital and add an s at the end when using scissors";
 }
+else if (inputForRockPaperScissors === "Win") {
+    document.getElementById("result").innerHTML = "You Thought you broke the system but it was me DIO!";
+}
+    else if (inputForRockPaperScissors === "Pistol") {
+    document.getElementById("result").innerHTML = "No... Just no.";
+}
+    else if (inputForRockPaperScissors === "Yeet") {
+    document.getElementById("result").innerHTML = "Yoink!";
+}
+    else if (inputForRockPaperScissors === "up up down down left right left right B A start") {
+    document.getElementById("result").innerHTML = "You won!!!!!";
+}
 else {
   document.getElementById("result").innerHTML = "Please use a valid input...";
 }
 
+document.getElementById("streak").innerHTML = streak;
+    
 }
